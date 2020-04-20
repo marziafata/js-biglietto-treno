915a0787a904f471aeafa10133ee8cc77afeaec2
Marzia Fatarella <marziafatarella@gmail.com>
2020-04-20 16:27:22 +0200

    stampato html

--- main.js
+++ main.js
@@ -8,34 +8,41 @@
 
 // chiedere numero chilometri
 var chilometri = prompt('Quanti km devi percorrere?');
-console.log('Chilometri da percorrere = ' + chilometri);
+document.getElementById('km').innerHTML='Devi percorrere: ' + chilometri + ' km';
+// console.log('Chilometri da percorrere = ' + chilometri);
+
 // chiedere eta passeggero
 var eta = prompt('Quanti anni hai?');
-console.log('Anni = ' + eta);
+// console.log('Anni = ' + eta);
+document.getElementById('eta').innerHTML='Hai: ' + eta + ' anni';
+
 // calcolare il prezzo totale del viaggio
 
 // tra i 18 e i 65 no sconto
 var prezzo_base = 0.21;
-console.log('Gli adulti tra i 18 e i 65 anni pagano per chilometro ' + prezzo_base);
+// console.log('Gli adulti tra i 18 e i 65 anni pagano per chilometro ' + prezzo_base.toFixed(2) + '€');
 
 // se > 65 - 40%
 var pensionato = (0.21 - ((0.21 / 100) * 40));
-console.log('Il pensionato paga per chilometro ' + pensionato);
+// console.log('Il pensionato paga per chilometro ' + pensionato.toFixed(2) + '€');
 
 // se < 18 - 20%
 var minorenne = (0.21 - ((0.21 / 100) * 20));
-console.log('Il minorenne paga per chilometro ' + minorenne);
+// console.log('Il minorenne paga per chilometro ' + minorenne.toFixed(2) + '€');
 
 if (eta >= 18 && eta < 65) {
     // tra i 18 e i 65 no sconto
-    totale = (chilometri * prezzo_base);
-    console.log('Hai tra i 18 e i 65 anni, quindi paghi ' + totale);
+    var totale = (chilometri * prezzo_base);
+    // console.log('Hai tra i 18 e i 65 anni, quindi paghi ' + totale.toFixed(2) + '€');
+    document.getElementById('paghi1').innerHTML='Il costo del tuo biglietto è: ' + totale.toFixed(2) + '€';
 } else if(eta >= 65) {
     // se >= 65 - 40%
-    totale2 = (chilometri * pensionato);
-    console.log('Hai più di 65 anni, quindi paghi ' + totale2);
+    var totale2 = (chilometri * pensionato);
+    // console.log('Hai più di 65 anni, quindi paghi ' + totale2.toFixed(2) + '€');
+    document.getElementById('paghi2').innerHTML='Il costo del tuo biglietto è: ' + totale2.toFixed(2) + '€';
 } else {
     // l'età inserita non è >=18 né >= 65
-    totale3 = (chilometri * minorenne);
-    console.log('Hai meno di 18 anni, quindi paghi ' + totale3);
+    var totale3 = (chilometri * minorenne);
+    // console.log('Hai meno di 18 anni, quindi paghi ' + totale3.toFixed(2)+ '€');
+    document.getElementById('paghi3').innerHTML='Il costo del tuo biglietto è: ' + totale3.toFixed(2) + '€';
 }
