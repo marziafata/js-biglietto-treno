50df2c1602ff0a503152f804845c4a10c2aeeff7
Marzia Fatarella <marziafatarella@gmail.com>
2020-04-21 10:25:40 +0200

    ottimizzazione codice con Sofia

--- main.js
+++ main.js
@@ -29,14 +29,14 @@ if (eta >= 18 && eta < 65) {
     document.getElementById('paghi1').innerHTML='Il costo del tuo biglietto è: ' + totale.toFixed(2) + '€';
 } else if(eta >= 65) {
     // se >= 65 - 40%
-    var pensionato = (0.21 - ((0.21 / 100) * 40));
+    var pensionato = (prezzo_base - ((prezzo_base / 100) * 40));
     // console.log('Il pensionato paga per chilometro ' + pensionato.toFixed(2) + '€');
     var totale2 = (chilometri * pensionato);
     // console.log('Hai più di 65 anni, quindi paghi ' + totale2.toFixed(2) + '€');
     document.getElementById('paghi2').innerHTML='Il costo del tuo biglietto scontato del 40% è: ' + totale2.toFixed(2) + '€';
 } else {
     // se < 18 - 20%
-    var minorenne = (0.21 - ((0.21 / 100) * 20));
+    var minorenne = (prezzo_base - ((prezzo_base / 100) * 20));
     // console.log('Il minorenne paga per chilometro ' + minorenne.toFixed(2) + '€');
     // l'età inserita non è >=18 né >= 65
     var totale3 = (chilometri * minorenne);
