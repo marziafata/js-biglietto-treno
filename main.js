5c1860a38cf4566bcdbb313ba0fba7e3d2426b7c
Marzia Fatarella <marziafatarella@gmail.com>
2020-04-21 10:17:38 +0200

    sistemazione codice con Sofia

--- main.js
+++ main.js
@@ -22,14 +22,6 @@ document.getElementById('eta').innerHTML='Hai: ' + eta + ' anni';
 var prezzo_base = 0.21;
 // console.log('Gli adulti tra i 18 e i 65 anni pagano per chilometro ' + prezzo_base.toFixed(2) + '€');
 
-// se > 65 - 40%
-var pensionato = (0.21 - ((0.21 / 100) * 40));
-// console.log('Il pensionato paga per chilometro ' + pensionato.toFixed(2) + '€');
-
-// se < 18 - 20%
-var minorenne = (0.21 - ((0.21 / 100) * 20));
-// console.log('Il minorenne paga per chilometro ' + minorenne.toFixed(2) + '€');
-
 if (eta >= 18 && eta < 65) {
     // tra i 18 e i 65 no sconto
     var totale = (chilometri * prezzo_base);
@@ -37,10 +29,15 @@ if (eta >= 18 && eta < 65) {
     document.getElementById('paghi1').innerHTML='Il costo del tuo biglietto è: ' + totale.toFixed(2) + '€';
 } else if(eta >= 65) {
     // se >= 65 - 40%
+    var pensionato = (0.21 - ((0.21 / 100) * 40));
+    // console.log('Il pensionato paga per chilometro ' + pensionato.toFixed(2) + '€');
     var totale2 = (chilometri * pensionato);
     // console.log('Hai più di 65 anni, quindi paghi ' + totale2.toFixed(2) + '€');
     document.getElementById('paghi2').innerHTML='Il costo del tuo biglietto scontato del 40% è: ' + totale2.toFixed(2) + '€';
 } else {
+    // se < 18 - 20%
+    var minorenne = (0.21 - ((0.21 / 100) * 20));
+    // console.log('Il minorenne paga per chilometro ' + minorenne.toFixed(2) + '€');
     // l'età inserita non è >=18 né >= 65
     var totale3 = (chilometri * minorenne);
     // console.log('Hai meno di 18 anni, quindi paghi ' + totale3.toFixed(2)+ '€');
